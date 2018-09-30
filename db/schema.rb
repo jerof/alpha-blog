3ed56488978cbec481f4e88dbe9e20974500de1e
Nikhil <nikhilsamuel.89@gmail.com>
2018-09-30 15:03:46 -0400

    Add user id to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_184211) do
+ActiveRecord::Schema.define(version: 2018_09_30_190131) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
