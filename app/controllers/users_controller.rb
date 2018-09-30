a74373cb9f3e85cc69ece0960da36ddeb4bfd9a1
Nikhil <nikhilsamuel.89@gmail.com>
2018-09-30 14:56:03 -0400

    Add secure password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Alphablog, #{@user.username}"
-      redirect_to user_path(@user)
+      redirect_to articles_path(@article)
     else
       render 'new'
     end
@@ -36,7 +37,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_170355) do
+ActiveRecord::Schema.define(version: 2018_09_30_184211) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_30_170355) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
