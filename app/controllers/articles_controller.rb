f121e079533334525be4226b99a0165adc30eb58
Nikhil <nikhilsamuel.89@gmail.com>
2018-10-01 09:35:42 -0400

    Add controller restrictions to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+before_action :require_user, except: [:index, :show]
+before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -18,11 +21,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -32,11 +33,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
@@ -45,6 +44,17 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 
 end
