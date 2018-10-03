582f0d4bf5dc674dd64a82d0729a636615c03f27
Nikhil <nikhilsamuel.89@gmail.com>
2018-10-03 13:27:41 -0400

    Add tests

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'bootstrap-sass', '~> 3.3.7'
 gem 'sass-rails', '>= 3.2'
 gem 'jquery-rails'
 gem 'bcrypt', '~> 3.1', '>= 3.1.12'
+gem 'rails-controller-testing'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
-class CatagoryTest < ActiveSupport::TestCase
-
+class CategoryTest < ActiveSupport::TestCase
   def setup
     @category = Category.new(name: "sports")
   end
@@ -10,4 +9,24 @@ class CatagoryTest < ActiveSupport::TestCase
     assert @category.valid?
   end
 
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
 end
